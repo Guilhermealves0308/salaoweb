a17433cd685d4c0ca97be9c957fff9e85dd2e93b
Guilherme alves <103002795+Guilhermealves0308@users.noreply.github.com>
2023-06-07 21:03:13 -0300

    first commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  nome: string = '';
+  preco: number = '';
 
+  constructor(private toastController: ToastController) { }
+
+  cadastrarProduto() {
+    // Aqui você pode adicionar a lógica para salvar o produto no banco de dados
+    // Por enquanto, apenas exibiremos um toast com os dados do produto
+    const produto = {
+      nome: this.nome,
+      preco: this.preco
+    };
+
+    this.presentToast(`Produto cadastrado: ${produto.nome}, Preço: R$${produto.preco}`);
+    this.nome = '';
+    this.preco = 0;
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'top'
+    });
+    toast.present();
+  }
 }
